41103ca6a0cd2652735816c4587f6a1c29887857
emor1 <57478311+emor1@users.noreply.github.com>
2024-03-09 13:24:01 +0900

    add mnist example and finished ch3

--- playground/loading_mnist_test.py
+++ playground/loading_mnist_test.py
@@ -1,1 +1,11 @@
 import os, sys
+sys.path.append(os.pardir)
+
+from dataset.mnist import load_mnist
+
+
+# 最初の呼び出し
+# (訓練画像、訓練ラベル), (テスト画像、テストラベル)
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+# load_mnist(normalizeにより０〜１に正規化、Falseにより255のまま、flattenにより1次元配列に)
