6248175726269a5d2f16a7b2b4e9f5d1b6948d5c
emor1 <57478311+emor1@users.noreply.github.com>
2024-03-19 23:55:06 +0900

    入力の描画機能を実装

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 """
 
 import sys
+import numpy as np
 import pygame
 from pygame.locals import *
 
@@ -12,17 +13,58 @@ screen = pygame.display.set_mode((600, 400))
 gray = (122,122,122)
 black = (0, 0, 0)
 
+input_x = 28
+input_y = 28
+
+input_field = [[(0,0,0)]*input_x for i in range(input_y)]
+print(input_field)
+
+cell_size = 10
+offset_x = 20
+offset_y = 50
+isPressed = False
+
+button_x = 100
+button = pygame.Rect(offset_x+((cell_size*input_x)/2)-button_x/2, offset_y+cell_size*input_y+10, button_x, 50)  # creates a rect object
+#STEP1.フォントの用意  
+font = pygame.font.SysFont(None, 25)
+
+#STEP2.テキストの設定
+text1 = font.render("Reset", True, (0,0,0))
 
 while True:
     # if pygame.mouse.get_pressed():
     screen.fill(gray)
+    pygame.draw.rect(screen, (200, 200, 200), button)
+    screen.blit(text1, (offset_x+((cell_size*input_x)/2)-25, offset_y+cell_size*input_y+25))
 
-    for i in range(28):
-        for j in range(28):
-            pygame.draw.rect(screen, black, (i*9+20, j*9+50, 8, 8), )
+    for i in range(input_x):
+        for j in range(input_y):
+            pygame.draw.rect(screen, input_field[i][j], (i*cell_size+offset_x, j*cell_size+offset_y, cell_size, cell_size), )
 
     pygame.display.update()
     for event in pygame.event.get():
+        if event.type ==MOUSEMOTION and isPressed:
+                mouse_Pos = pygame.mouse.get_pos()
+                if mouse_Pos[0] >= cell_size*input_x+offset_x or mouse_Pos[1] >= cell_size*input_y + offset_y or mouse_Pos[0] < offset_x or mouse_Pos[1] < offset_y:
+                    continue
+                x = int((mouse_Pos[0]-offset_x)/cell_size)
+                y = int((mouse_Pos[1]-offset_y)/cell_size)
+                input_field[x][y] = (255,255,255)
+
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                isPressed = True
+
+            if button.collidepoint(event.pos):
+                input_field = [[(0,0,0)]*input_x for i in range(input_y)]
+
+        elif event.type == MOUSEBUTTONUP:
+            isPressed = False
+                # print(x, y)
+                # print(input_field)
+
+
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
